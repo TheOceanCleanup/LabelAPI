922eb3136c1885317171bf3511be6b97f7cdfb2c
Jayke Meijer <jayke@xomnia.com>
2020-10-14 13:45:18 +0200

    Being consistent with tab depth

--- app/tests/test_auth_failures.py
+++ app/tests/test_auth_failures.py
@@ -5,32 +5,32 @@ from tests.shared import get_headers
 
 
 def test_invalid_key(client, app, db, mocker):
-  headers = get_headers(db)
+    headers = get_headers(db)
 
-  headers["Authentication-Key"] = "124"
-  response = client.get("/api/v1/images", headers=headers)
-  assert response.status_code == 401
+    headers["Authentication-Key"] = "124"
+    response = client.get("/api/v1/images", headers=headers)
+    assert response.status_code == 401
 
 
 def test_invalid_secret(client, app, db, mocker):
-  headers = get_headers(db)
+    headers = get_headers(db)
 
-  headers["Authentication-Secret"] = "Foobaz"
-  response = client.get("/api/v1/images", headers=headers)
-  assert response.status_code == 401
+    headers["Authentication-Secret"] = "Foobaz"
+    response = client.get("/api/v1/images", headers=headers)
+    assert response.status_code == 401
 
 
 def test_missing_api_key(client, app, db, mocker):
-  headers = get_headers(db)
+    headers = get_headers(db)
 
-  del headers["Authentication-Key"]
-  response = client.get("/api/v1/images", headers=headers)
-  assert response.status_code == 401
+    del headers["Authentication-Key"]
+    response = client.get("/api/v1/images", headers=headers)
+    assert response.status_code == 401
 
 
 def test_missing_api_secret(client, app, db, mocker):
-  headers = get_headers(db)
+    headers = get_headers(db)
 
-  del headers["Authentication-Secret"]
-  response = client.get("/api/v1/images", headers=headers)
-  assert response.status_code == 401
+    del headers["Authentication-Secret"]
+    response = client.get("/api/v1/images", headers=headers)
+    assert response.status_code == 401
