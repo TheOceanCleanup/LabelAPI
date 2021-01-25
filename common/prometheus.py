8468edce1d58017b50686485a05685f92768961f
avollering <44085641+avollering@users.noreply.github.com>
2021-01-25 12:49:34 +0100

    Added "label_storage" as a prefix to the custom metrics and adjusted the deployment manifests, see CL-461

--- common/prometheus.py
+++ common/prometheus.py
@@ -3,15 +3,19 @@ from prometheus_client import Counter, Gauge, Histogram, CollectorRegistry, mult
 registry = CollectorRegistry()
 multiprocess.MultiProcessCollector(registry)
 
-number_of_labeled_images            = Counter("number_of_labeled_images", "Number of labeled images",
+number_of_labeled_images            = Counter("label_storage_number_of_labeled_images",
+                                              "Number of labeled images in the label storage",
                                               registry=registry)
-number_of_bounding_boxes_per_image  = Histogram("number_of_bounding_boxes_per_image",
-                                                "Number of bounding boxes per image",
+number_of_bounding_boxes_per_image  = Histogram("label_storage_number_of_bounding_boxes_per_image",
+                                                "Number of bounding boxes per image in the label storage",
                                                 registry=registry)
-number_of_available_images          = Counter("number_of_available_images", "Number of available images",
+number_of_available_images          = Counter("label_storage_number_of_available_images",
+                                              "Number of available images in the label storage",
                                               registry=registry)
-number_of_unlabeled_images          = Gauge("number_of_unlabeled_images", "Number of unlabeled images",
+number_of_unlabeled_images          = Gauge("label_storage_number_of_unlabeled_images",
+                                            "Number of unlabeled images in the label storage",
                                             multiprocess_mode='livesum',
                                             registry=registry)
-total_storage_container_size        = Counter("total_storage_container_size", "Total size of the storage container",
+total_storage_container_size        = Counter("label_storage_total_storage_container_size",
+                                              "Total size of the storage container in the label storage",
                                               registry=registry)
